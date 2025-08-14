a6bc4defb79137e44adc93ee3692fd6e8ae6d27c
Kaiting Chen <ktchen14@gmail.com>
2025-08-14 18:41:20 -0400

    Add Svelte filetype configuration

--- vim/plugin/nvim.lua
+++ vim/plugin/nvim.lua
@@ -116,6 +116,8 @@ end, { desc = 'vim.lsp.buf.declaration()' })
 local ok, lspconfig = pcall(require, 'lspconfig')
 if ok then
   lspconfig.clangd.setup {}
+  lspconfig.ts_ls.setup {}
+  lspconfig.svelte.setup {}
 end
 
 -- nvim-treesitter
@@ -123,6 +125,7 @@ end
 local ok, treesitter = pcall(require, 'nvim-treesitter.configs')
 if ok then
   treesitter.setup {
+    ensure_installed = { 'css', 'html', 'javascript', 'svelte' },
     highlight = { enable = true },
   }
 end
